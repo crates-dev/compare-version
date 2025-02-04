4991818003486993555c00ec0ee7c893239bf5fe
尤雨东 <root@ltpp.vip>
2025-02-04 10:29:31 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add jihulab ssh://jihulab.com:ltpp/compare_version.git;
+git remote add jihulab git@jihulab.com:ltpp-universe/compare_version.git;
 git remote add github git@github.com:ltpp-universe/compare_version.git;
 git remote add origin git@github.com:ltpp-universe/compare_version.git;
