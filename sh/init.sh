f4f815d236a893aa29a675f8a8c240acde02e5c5
尤雨东 <root@ltpp.vip>
2025-02-07 23:10:08 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+git init;
+git config core.ignorecase false;
 git remote add gitee git@gitee.com:ltpp-universe/compare_version.git;
-git remote add github git@github.com:ltpp-universe/compare_version.git;
 git remote add origin git@github.com:ltpp-universe/compare_version.git;
