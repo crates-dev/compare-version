da86d1340df9cf45793284fcd91da2b6fa94e62a
尤雨东 <root@ltpp.vip>
2026-01-21 09:33:20 +0800

    feat: v2.0.1

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,12 @@
 //! A Rust library for comparing semantic versioning
 //! strings and checking version compatibility.
 
-pub(crate) mod r#enum;
-pub(crate) mod r#impl;
-pub(crate) mod r#struct;
-
+mod r#enum;
+mod r#impl;
+mod r#struct;
 #[cfg(test)]
 mod test;
 
 pub use {r#enum::*, r#struct::*};
 
-pub(crate) use std::fmt;
+use std::fmt;
